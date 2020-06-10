46c576245f6605b1a073394e2d992db525e1b138
Atharva <46068146+atharvamh@users.noreply.github.com>
2020-06-10 18:35:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from VGG16 import *
+from data_preprocess import *
