37fd568ae64e9ec83621bbc470f83f73d912c837
Atharva <46068146+atharvamh@users.noreply.github.com>
2020-06-10 18:43:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ from VGG16 import *
 from data_preprocess import *
 
 if __name__ == "__main__":
+  vgg16_arch()
+  opt = Adam(lr=0.001)
+  model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy',tf.keras.metrics.Precision(),tf.keras.metrics.Recall()])
+  checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+  early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
   
